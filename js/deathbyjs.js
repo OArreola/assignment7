d404130957596a4aa1b52dcc5328c0f13d1f95d3
OArreola <orlando_arreola@hotmail.com>
2020-04-08 23:33:42 -0700

    Added Part 4 - Death by JavaScript

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,11 +1,92 @@
 /*eslint-env browser*/
 
-//STEP 1
+$("secDeath").addEventListener("click", (event) => {
+  "use strict";
 
-//STEP 2
+  //STEP 1
+  function step1() {
+    "use strict";
+    let str = window.prompt("Enter a string:");
+    window.console.log(str.match(/./g).sort().join().replace(/\W/g, ""));
+  }
 
-//STEP 3
+  //STEP 2
+  function step2() {
+    "use strict";
+    let str = window.prompt("Enter a string:");
+    let arrStr = str.split(" ");
+    let arrStr2 = [];
+    for (let i of arrStr) {
+      arrStr2.push(i.charAt(0).toUpperCase().concat(i.slice(1)));
+    }
+    window.console.log(arrStr2.join(" "));
+  }
 
-//STEP 4
+  //STEP 3
+  //'The quick brown fox'
+  //THE QUICK FOX BROWN
+  function step3() {
+    "use strict";
+    let str = window.prompt("Enter a string:");
+    let res = str.match(/[aeiouAEIOU]/g);
+    window.console.log("The total count of vowels is " + res.length);
+  }
 
-//STEP 5
+  //STEP 4
+  let SIZEOFID = 8; //Size of ID in chars
+  function step4() {
+    "use strict";
+    let chars =
+      "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let id = "";
+    for (let i = 0; i < SIZEOFID; i++) {
+      id = id.concat(chars.charAt(Math.floor(Math.random() * chars.length)));
+    }
+    window.console.log(id);
+  }
+
+  //STEP 5
+  //Australia|Germany|United States of America
+  function longest_country_name(arr) {
+    let longest = arr.sort(function (a, b) {
+      //window.console.log(a, b);
+      return b.length - a.length;
+    })[0];
+    return longest;
+  }
+  function step5() {
+    "use strict";
+    let str = window.prompt(
+      'Enter a string of names of countries separated by "|"\ni.e: Australia|Germany|United States of America'
+    );
+    if (str !== null) {
+      //CONVERT STRING TO ARRAY
+      let arrStr = str.split("|");
+      window.console.log("The longest name is " + longest_country_name(arrStr));
+    }
+  }
+
+  const isButton = event.target.nodeName === "BUTTON";
+  if (isButton) {
+    //window.console.log(event.target.innerHTML);
+    switch (event.target.innerHTML) {
+      case "Step 1":
+        step1();
+        break;
+      case "Step 2":
+        step2();
+        break;
+      case "Step 3":
+        step3();
+        break;
+      case "Step 4":
+        step4();
+        break;
+      case "Step 5":
+        step5();
+        break;
+      default:
+        break;
+    }
+  }
+});
--- js/strings.js
+++ js/strings.js
@@ -1,142 +1,145 @@
 /*eslint-env browser*/
 
-//STEP 1
-function step1() {
-  "use strict";
-  let nameStr = window.prompt("Enter your name: ");
-  window.alert("Your name is " + nameStr.length + " characters long.");
-}
-
-//STEP 2
-function step2() {
-  "use strict";
-  let nameStr = window.prompt("Enter your name: ");
-  let pos = 0;
-  do {
-    pos = window.prompt("Enter a number between 1 and " + nameStr.length);
-    if (pos < 1 || pos > nameStr.length) {
-      window.alert("Wrong number!!!");
-    }
-  } while (pos < 1 || pos > nameStr.length);
-  window.alert(
-    "At position " +
-      pos +
-      " of " +
-      nameStr +
-      " is [ " +
-      nameStr.charAt(pos - 1) +
-      " ]"
-  );
-}
-//STEP 3
-function step3() {
-  "use strict";
-  let first = window.prompt("Enter your First Name:");
-  let last = window.prompt("Enter your Last Name:");
-  window.alert("Your name is:\n" + first.concat(" " + last));
-}
-
-//STEP 4
-function step4() {
-  "use strict";
-  let txt = "The quick brown fox jumps over the lazy dog";
-  window.alert(txt + "\nThe word fox is at index: " + txt.indexOf("fox"));
-}
-
-//STEP 5
-function step5() {
-  "use strict";
-  let txt = "The quick brown fox jumps over the lazy fox";
-  window.alert(
-    txt + "\nThe last instance of fox is at index: " + txt.lastIndexOf("fox")
-  );
-}
-
-//STEP 6
-function step6() {
-  "use strict";
-  let txt = "The quick brown fox jumps over the lazy dog";
-  let name = window.prompt("Enter your name:");
-  window.alert(txt.replace("the lazy dog", name));
-}
-
-//STEP 7
-function step7() {
-  "use strict";
-  let txt = "The quick brown fox jumps over the lazy dog";
-  let word = window.prompt("Enter a word:");
-  let res = txt.search(word);
-  if (res === -1) {
-    window.alert("Sorry, " + word + " is not in\n" + txt);
-  } else {
-    window.alert(
-      "The word " + word + " is located at index " + res + " of\n" + txt
-    );
-  }
-}
-
-//STEP 8
-function step8() {
-  "use strict";
-  let old_string = "The quick brown fox jumps over the lazy dog";
-  let new_string = old_string.slice(old_string.search("the lazy dog"));
-  window.alert(new_string.toUpperCase());
-}
-
-//STEP 9
-function step9() {
-  "use strict";
-  let txt = "            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ";
-  window.alert(txt.trim().toLowerCase());
-}
-
-//STEP 10
-function step10() {
-  "use strict";
-  let txt = "the quick brown fox jumps over the lazy dog";
-
-  window.alert(txt.slice(0, 1).toUpperCase().concat(txt.slice(1, txt.length)));
-}
-
-//General
 let $ = function (id) {
   "use strict";
   return window.document.getElementById(id);
 };
+
 $("secStrings").addEventListener("click", (event) => {
   "use strict";
+
+  //STEP 1
+  function step1() {
+    "use strict";
+    let nameStr = window.prompt("Enter your name: ");
+    window.alert("Your name is " + nameStr.length + " characters long.");
+  }
+
+  //STEP 2
+  function step2() {
+    "use strict";
+    let nameStr = window.prompt("Enter your name: ");
+    let pos = 0;
+    do {
+      pos = window.prompt("Enter a number between 1 and " + nameStr.length);
+      if (pos < 1 || pos > nameStr.length) {
+        window.alert("Wrong number!!!");
+      }
+    } while (pos < 1 || pos > nameStr.length);
+    window.alert(
+      "At position " +
+        pos +
+        " of " +
+        nameStr +
+        " is [ " +
+        nameStr.charAt(pos - 1) +
+        " ]"
+    );
+  }
+  //STEP 3
+  function step3() {
+    "use strict";
+    let first = window.prompt("Enter your First Name:");
+    let last = window.prompt("Enter your Last Name:");
+    window.alert("Your name is:\n" + first.concat(" " + last));
+  }
+
+  //STEP 4
+  function step4() {
+    "use strict";
+    let txt = "The quick brown fox jumps over the lazy dog";
+    window.alert(txt + "\nThe word fox is at index: " + txt.indexOf("fox"));
+  }
+
+  //STEP 5
+  function step5() {
+    "use strict";
+    let txt = "The quick brown fox jumps over the lazy fox";
+    window.alert(
+      txt + "\nThe last instance of fox is at index: " + txt.lastIndexOf("fox")
+    );
+  }
+
+  //STEP 6
+  function step6() {
+    "use strict";
+    let txt = "The quick brown fox jumps over the lazy dog";
+    let name = window.prompt("Enter your name:");
+    window.alert(txt.replace("the lazy dog", name));
+  }
+
+  //STEP 7
+  function step7() {
+    "use strict";
+    let txt = "The quick brown fox jumps over the lazy dog";
+    let word = window.prompt("Enter a word:");
+    let res = txt.search(word);
+    if (res === -1) {
+      window.alert("Sorry, " + word + " is not in\n" + txt);
+    } else {
+      window.alert(
+        "The word " + word + " is located at index " + res + " of\n" + txt
+      );
+    }
+  }
+
+  //STEP 8
+  function step8() {
+    "use strict";
+    let old_string = "The quick brown fox jumps over the lazy dog";
+    let new_string = old_string.slice(old_string.search("the lazy dog"));
+    window.alert(new_string.toUpperCase());
+  }
+
+  //STEP 9
+  function step9() {
+    "use strict";
+    let txt =
+      "            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ";
+    window.alert(txt.trim().toLowerCase());
+  }
+
+  //STEP 10
+  function step10() {
+    "use strict";
+    let txt = "the quick brown fox jumps over the lazy dog";
+
+    window.alert(
+      txt.slice(0, 1).toUpperCase().concat(txt.slice(1, txt.length))
+    );
+  }
+
   const isButton = event.target.nodeName === "BUTTON";
   if (isButton) {
-    //window.console.log(event.target.id);
-    switch (event.target.id) {
-      case "step1":
+    switch (event.target.innerHTML) {
+      case "Step 1":
         step1();
         break;
-      case "step2":
+      case "Step 2":
         step2();
         break;
-      case "step3":
+      case "Step 3":
         step3();
         break;
-      case "step4":
+      case "Step 4":
         step4();
         break;
-      case "step5":
+      case "Step 5":
         step5();
         break;
-      case "step6":
+      case "Step 6":
         step6();
         break;
-      case "step7":
+      case "Step 7":
         step7();
         break;
-      case "step8":
+      case "Step 8":
         step8();
         break;
-      case "step9":
+      case "Step 9":
         step9();
         break;
-      case "step10":
+      case "Step 10":
         step10();
         break;
 
